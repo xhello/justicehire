dd368e42b534c28e92f6da9fa7b11f61258d3325
Weijyuan <weijyuan@justicehire.com>
2026-01-17 23:22:02 -0800

    Make inactive tab border thicker (border-2)

--- app/business/[id]/Tabs.tsx
+++ app/business/[id]/Tabs.tsx
@@ -30,7 +30,7 @@ export default function Tabs({ tabs, defaultTab }: TabsProps) {
                 ${
                   activeTab === tab.id
                     ? 'bg-blue-600 text-white hover:bg-blue-700'
-                    : 'bg-white text-blue-600 border border-blue-600 hover:bg-blue-50'
+                    : 'bg-white text-blue-600 border-2 border-blue-600 hover:bg-blue-50'
                 }
               `}
             >
